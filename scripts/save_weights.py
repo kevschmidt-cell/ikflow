5a33fdba6d22ee55aeff6aeb0d3b657fa23730af
Kevin Schmidt <kevschmidt9@gmail.com>
2025-07-17 20:57:25 +0200

    save weights locally

---
+++ scripts/save_weights.py
@@ -0,0 +1,4 @@
+import shutil
+
+model_path = "/home/kevin/ikflow/iiwa7_L__likely-monkey-2__global_step=0.75M.pkl"
+shutil.copy(model_path, "/home/kevin/iiwa7_L__likely-monkey-2__global_step=0.75M.pkl")
